7d09dab007bf59f0493493bcd7438dd57674e783
SebastienNin <sebastien.nin@hotmail.com>
2023-08-07 16:26:06 +0200

    Add function from DAOCompte into IDAOCompte

--- EscapeGame_back/src/main/java/escapeGame/dao/IDAOCompte.java
+++ EscapeGame_back/src/main/java/escapeGame/dao/IDAOCompte.java
@@ -1,7 +1,11 @@
 package escapeGame.dao;
 
+import java.util.List;
+
 import escapeGame.model.Compte;
+import escapeGame.model.GameMaster;
 
 public interface IDAOCompte extends IDAO<Compte,Integer>{
-
+	public Compte findByLoginAndPassword(String login, String password);
+	public List<GameMaster> findAllGameMaster();
 }
