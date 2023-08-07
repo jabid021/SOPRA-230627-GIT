2a034df7eab9e9eb7a164dd97d748cfb6d43b176
Anna <anna.jouannic35@gmail.com>
2023-08-07 13:03:58 +0200

    ajout controller Ordinateur

---
+++ quest-front/src/main/java/controller/OrdinateurController.java
@@ -0,0 +1,88 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import context.Singleton;
+import dao.IDAOStagiaire;
+import dao.IDAOOrdinateur;
+import model.Stagiaire;
+import model.Ordinateur;
+
+
+@WebServlet("/ordinateur")
+public class OrdinateurController extends HttpServlet {
+
+	private IDAOOrdinateur daoOrdinateur = Singleton.getInstance().getDaoOrdinateur();
+	private IDAOStagiaire daoStagiaire = Singleton.getInstance().getDaoStagiaire();
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+		if(request.getParameter("id")==null) {
+			List<Ordinateur> ordinateurs = daoOrdinateur.findAll();
+			List<Stagiaire> stagiaires = daoStagiaire.findAll();
+			request.setAttribute("ordinateurs", ordinateurs);
+			request.setAttribute("stagiaires", stagiaires);
+			this.getServletContext().getRequestDispatcher("/WEB-INF/ordinateurs.jsp").forward(request, response);
+		}
+		else 
+		{
+			if(request.getParameter("delete")==null) 
+			{
+				Integer id = Integer.parseInt(request.getParameter("id"));
+				Ordinateur ordinateur = daoOrdinateur.findById(id);
+				List<Stagiaire> stagiaires = daoStagiaire.findAll();
+				request.setAttribute("ordinateur", ordinateur);
+				request.setAttribute("stagiaires", stagiaires);
+				this.getServletContext().getRequestDispatcher("/WEB-INF/updateOrdinateur.jsp").forward(request, response);
+			}
+			else 
+			{
+				Integer id = Integer.parseInt(request.getParameter("id"));
+				daoOrdinateur.delete(id);
+				response.sendRedirect("ordinateur");
+			}
+		}
+
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("id")==null) 
+		{
+			String marque = request.getParameter("marque");
+			int ram = Integer.parseInt(request.getParameter("ram"));
+			Integer idStagiaire = Integer.parseInt(request.getParameter("stagiaire"));
+			Stagiaire stagiaire = daoStagiaire.findById(idStagiaire);
+			
+			Ordinateur ordinateur = new Ordinateur(marque, ram, stagiaire);
+			
+			
+			daoOrdinateur.insert(ordinateur);
+			response.sendRedirect("ordinateur");
+		}
+		else 
+		{
+			Integer id = Integer.parseInt(request.getParameter("id"));
+			String marque = request.getParameter("marque");
+			int ram = Integer.parseInt(request.getParameter("ram"));
+			Integer idStagiaire = Integer.parseInt(request.getParameter("stagiaire"));
+			Stagiaire stagiaire = daoStagiaire.findById(idStagiaire);
+			
+			Ordinateur ordinateur = new Ordinateur(id,marque, ram, stagiaire);
+			
+			
+			daoOrdinateur.update(ordinateur);
+			response.sendRedirect("ordinateur");;
+		}
+	}
+
+}
--- quest-front/src/main/webapp/WEB-INF/ordinateurs.jsp
+++ quest-front/src/main/webapp/WEB-INF/ordinateurs.jsp
@@ -1,12 +1,82 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@include file = "/WEB-INF/include.jsp" %>
+
+    
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Liste des Ordinateurs</title>
 </head>
 <body>
 
+
+<div id="content">
+  <h1>Liste des Ordinateurs</h1>
+  <input id="btnAddOrdinateur" type="button" class ="btn btn-success" value="Ajouter">
+  <a href="index.html"><input type="button" class ="btn btn-info" value="Retour"></a>
+
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Marque</th>
+        <th>Ram</th>
+        <th>Stagiaire</th>
+      </tr>
+    </thead>
+    <tbody>
+    
+    <c:forEach items="${ordinateurs}" var="ordinateur">
+    
+    <tr>
+        <td>${ordinateur.id}</td>
+       	<td>${ordinateur.marque}</td>
+        <td>${ordinateur.ram}</td>
+        <td>${ordinateur.stagiaire.id} - ${ordinateur.stagiaire.nom} - ${ordinateur.stagiaire.prenom}</td>
+        <td>
+          <a href="ordinateur?id=${ordinateur.id}"><input type="button" class ="btn btn-warning" value="Modifier"></a>
+          <a href="ordinateur?id=${ordinateur.id}&delete"><input type="button" class ="btn btn-danger" value="Supprimer"></a>
+        </td>
+      </tr>
+    </c:forEach>
+    </tbody>
+  </table>
+
+
+
+
+
+  <div id="addFormOrdinateur" class="formAjout">
+    <h3>Ajouter Ordinateur</h3>
+    <form action="ordinateur" method="post">
+      Marque :<input name="marque" type="text" placeholder="Saisir la marque"><br>
+      Ram :<input name="ram" type="number" placeholder="Saisir la ram"><br>
+      Stagiaire
+      <select name="stagiaire">
+      	<c:forEach items="${stagiaires}" var="stagiaire">
+      		<option value="${stagiaire.id}" >${stagiaire.id} - ${stagiaire.nom} - ${stagiaire.prenom}</option>
+      	</c:forEach>
+      </select><br>
+      <input class ="btn btn-success" type="submit" value="Ajouter">
+    </form>
+  </div>
+
+</div>
+
+
+
+
 </body>
 </html>
+
+
+
+<script>
+
+  btnAddOrdinateur.onclick=function()
+  {
+    addFormOrdinateur.style.display="block";
+  }
+
+</script>
+
