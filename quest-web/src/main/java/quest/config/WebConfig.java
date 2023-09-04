1d2bef6763eed9cb9ecc95328095eb098a79cf76
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2023-09-04 15:31:49 +0200

    quest-web : controlleur début

---
+++ quest-web/src/main/java/quest/config/WebConfig.java
@@ -0,0 +1,16 @@
+package quest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("quest.controller")
+public class WebConfig implements WebMvcConfigurer {
+	public void addResourceHandlers(ResourceHandlerRegistry registry ) {
+		registry.addResourceHandler ("/assets/**").addResourceLocations("/WEB-INF/assets/");
+	}
+}
